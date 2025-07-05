0150e53ec8c9bafd9cd8577b45f9036d642f9d3b
Abhinav Vats <142833016+Avisav24@users.noreply.github.com>
2025-07-05 21:53:38 +0530

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -34,6 +34,7 @@ const Login = () => {
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
                   Email Address
                 </label>
+                
                 <div className="relative">
                   <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                   <input
